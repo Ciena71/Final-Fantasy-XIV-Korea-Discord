b36319a4c59c502f0576f91d18987e9fdbe4e2d6
Ciena71 <63537327+Ciena71@users.noreply.github.com>
2021-08-14 16:56:46 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -2096,7 +2096,6 @@ client.on("interactionCreate", async (interaction) =>
 										.setTitle(jobSkill[i].name.replace(/`/gi,"\n"))
 										.setDescription(jobSkill[i].description.replace(/`/gi,"\n"))
 										.setThumbnail(jobSkill[i].icon);
-										//.setFooter("\u2063" + "\u2002".repeat(30)+"\u2063|");
 										if(jobSkill[i].type != null)
 											Embed.addField("종류", jobSkill[i].type, true)
 										else
@@ -2134,7 +2133,6 @@ client.on("interactionCreate", async (interaction) =>
 										.setTitle(roleSkill[i].name.replace(/`/gi,"\n"))
 										.setDescription(roleSkill[i].description.replace(/`/gi,"\n"))
 										.setThumbnail(roleSkill[i].icon);
-										//.setFooter("\u2063" + "\u2002".repeat(30)+"\u2063|");
 										if(roleSkill[i].type != null)
 											Embed.addField("종류", roleSkill[i].type, true)
 										else
@@ -2173,7 +2171,6 @@ client.on("interactionCreate", async (interaction) =>
 										.setDescription(jobTrait[i].description.replace(/`/gi,"\n"))
 										.setThumbnail(jobTrait[i].icon)
 										.addField("요구 레벨", jobTrait[i].level);
-										//.setFooter("\u2063" + "\u2002".repeat(30)+"\u2063|");
 										interaction.channel.send({ embeds: [Embed] });
 									}
 									interaction.editReply({ content: "정상적으로 생성되었습니다." });
@@ -4892,7 +4889,6 @@ async function loadFile(msg, url)
 							break;
 						}
 					}
-					setRole(msg, msg.member, msg.guild.roles.cache.find(r => r.name === "⁣           데이터 센터           ⁣"));
 					msg.editReply({ content: "성공적으로 인증되었습니다", ephemeral: true });
 					const Embed = new Discord.MessageEmbed()
 					.setColor('#ff00ff')
