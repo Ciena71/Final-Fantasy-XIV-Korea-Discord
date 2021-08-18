ab657489263882f4acbe066b1934c083491e8fbe
Ciena71 <63537327+Ciena71@users.noreply.github.com>
2021-08-18 20:33:27 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3460,38 +3460,24 @@ client.on("messageCreate", async (msg) =>
 									{
 										if(res.rows.length > 0)
 										{
-											msg.guild.channels.cache.get(channelsId.dialog).threads.fetch(res.rows[0].dialog).then(dialogthread =>
+											msg.guild.channels.cache.get(channelsId.dialog).threads.create(
 											{
-												if(dialogthread)
+												name: target.id,
+												autoArchiveDuration: 60,
+												//type: 'private_thread',
+												reason: target.user.tag + "님의 다이얼로그 생성"
+											})
+											.then(threadChannel => 
+											{
+												dataBase.query("UPDATE UserSaveData SET Dialog = '" + threadChannel.id + "' WHERE User_Id='" + target.id +"'");
+												threadChannel.members.add(target.id);
+												FFXIV_Guild.channels.cache.get(channelsId.dialog).messages.fetch(threadChannel.id).then(threadmsg =>
 												{
-													dialogthread.setArchived(false).then(() =>
-													{
-														dialogthread.delete();
-														msg.guild.channels.cache.get(channelsId.dialog).threads.create(
-														{
-															name: target.id,
-															autoArchiveDuration: 60,
-															//type: 'private_thread',
-															reason: target.user.tag + "님의 다이얼로그 생성"
-														})
-														.then(threadChannel => 
-														{
-															dataBase.query("UPDATE UserSaveData SET Dialog = '" + threadChannel.id + "' WHERE User_Id='" + target.id +"'");
-															threadChannel.members.add(target.id);
-															FFXIV_Guild.channels.cache.get(channelsId.dialog).messages.fetch(threadChannel.id).then(threadmsg =>
-															{
-																threadmsg.delete();
-															});
-														})
-														.catch(console.error);
-														msg.reply("<@" + target.id + ">님의 다이얼로그를 재연결했습니다.").then(message => { setTimeout(() => message.delete(), 10000); });
-													});
-												}
-											}).catch(err1 =>
-											{
-												console.log(err1);
-												msg.reply("해당 <@" + target.id + ">의 데이터에 오류가 났습니다. 에러코드 : 2").then(message => { setTimeout(() => message.delete(), 10000); });
-											});
+													threadmsg.delete();
+												});
+											})
+											.catch(console.error);
+											msg.reply("<@" + target.id + ">님의 다이얼로그를 재연결했습니다.").then(message => { setTimeout(() => message.delete(), 10000); });
 										}
 										else
 										msg.reply("해당 <@" + target.id + ">의 데이터가 존재하지 않습니다.").then(message => { setTimeout(() => message.delete(), 10000); });
