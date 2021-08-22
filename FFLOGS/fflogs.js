fbf845c8b89ee2cf2ade63b8f43f85b5e89462f1
Ciena71 <63537327+Ciena71@users.noreply.github.com>
2021-08-23 03:05:16 +0900

    Update fflogs.js

--- FFLOGS/fflogs.js
+++ FFLOGS/fflogs.js
@@ -1,7 +1,27 @@
+import { buildSdk } from "@rpglogs/api-sdk";
+
+const ffSdk = buildSdk(accessToken, 'ff');
+
+function getUserZoneRanking(zone_id, character_name, server, region)
+{
+    const response = await ffSdk.getCharacterZoneRankings(
+    {
+        zoneId: zone_id,
+        characterName: character_name,
+        characterServerSlug: server,
+        characterRegion: region,
+        metric: 'rdps',
+        difficulty: 101,
+        partition: -1,
+    });
+    return response.characterData.character.zoneRankings;
+}
+
+/*
 function request_code(req, res, next)
 {
     const authurl = "https://www.fflogs.com/oauth/authorize";
     const tokkenurl = "https://www.fflogs.com/oauth/token";
     //process.env.FFLOGSID
     //process.env.FFLOGSPW
-}
+}*/
